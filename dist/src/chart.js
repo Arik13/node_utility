e9fc68db975d0dff3ce7f55d4b66e08bc0c5b40a
Arik Dicks <ariksven@gmail.com>
2022-04-18 04:27:20 -0700

    Fixed package vulnerabilities

--- dist/src/chart.js
+++ dist/src/chart.js
@@ -1,112 +1,125 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Chart = void 0;
-const Chart1 = require("chart.js-image");
-const fs = require("fs");
-let chartData = (title, data) => ({
-    type: "scatter",
-    data: {
-        datasets: [
-            {
-                label: title,
-                data
-            },
-        ]
-    },
-    options: {
-        title: {
-            display: false,
-            text: "Chart.js Line Chart"
-        },
-        scales: {
-            xAxes: [
-                {
-                    stacked: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: "X"
-                    }
-                }
-            ],
-            yAxes: [
-                {
-                    stacked: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Y"
-                    }
-                }
-            ]
-        }
-    }
-});
-class Chart {
-    constructor(options) {
-        // @ts-ignore
-        let c = new Chart1(options);
-        this.ch = c;
-    }
-    backgroundColor(value) {
-        return Chart.init(this.ch.backgroundColor(value));
-    }
-    bkg(value) {
-        return Chart.init(this.ch.bkg(value));
-    }
-    c(value) {
-        return Chart.init(this.ch.c(value));
-    }
-    encoding(value) {
-        return Chart.init(this.ch.encoding(value));
-    }
-    icac(value) {
-        return Chart.init(this.ch.icac(value));
-    }
-    ichm(value) {
-        return Chart.init(this.ch.ichm(value));
-    }
-    icretina(value) {
-        return Chart.init(this.ch.icretina(value));
-    }
-    toURL(value) {
-        return Chart.init(this.ch.toURL(value));
-    }
-    height(value) {
-        return Chart.init(this.ch.height(value));
-    }
-    width(value) {
-        return Chart.init(this.ch.width(value));
-    }
-    chart(value) {
-        return Chart.init(this.ch.chart(value));
-    }
-    toBuffer() { return this.ch.toBuffer(); }
-    toDataURI(string) { return this.ch.toDataURI(string); }
-    plot(data, title, color, width, height) {
-        let c = this.chart(chartData(title, data.map(x => ({ x: x[0], y: x[1] }))))
-            .backgroundColor(color)
-            .width(width)
-            .height(height);
-        return Chart.init(c);
-    }
-    write(path) {
-        return __awaiter(this, void 0, void 0, function* () {
-            fs.writeFileSync(path, yield this.toBuffer());
-        });
-    }
-    static init(c) {
-        let chart = new Chart();
-        chart.ch = c;
-        return chart;
-    }
-}
-exports.Chart = Chart;
+// import * as Chart1 from 'chart.js-image';
+// import * as fs from 'fs';
+// let chartData = (title: string, data: {x: number, y: number}[]) => ({
+//     type: "scatter",
+//     data: {
+//         datasets: [
+//             {
+//                 label: title,
+//                 data
+//             },
+//         ]
+//     },
+//     options: {
+//         title: {
+//             display: false,
+//             text: "Chart.js Line Chart"
+//         },
+//         scales: {
+//             xAxes: [
+//                 {
+//                     stacked: true,
+//                     scaleLabel: {
+//                         display: true,
+//                         labelString: "X"
+//                     }
+//                 }
+//             ],
+//             yAxes: [
+//                 {
+//                     stacked: true,
+//                     scaleLabel: {
+//                         display: true,
+//                         labelString: "Y"
+//                     }
+//                 }
+//             ]
+//         }
+//     }
+// })
+// export interface ChartOptions {
+//     timeout?: number;
+//     secret?: string;
+//     host?: string;
+//     protocol?: string;
+//     port?: number;
+//     pathname?: string;
+// }
+// export interface I_Chart {
+//     new(options: ChartOptions): I_Chart;
+//     backgroundColor(string: string): I_Chart;
+//     bkg(string: string): I_Chart;
+//     c(string: string): I_Chart;
+//     chart(string: string | any): I_Chart;
+//     encoding(string: string): I_Chart;
+//     icac(string: string): I_Chart;
+//     ichm(string: string): I_Chart;
+//     icretina(string: string): I_Chart;
+//     toBuffer(): Promise<Buffer>;
+//     toDataURI(string: string): Promise<string>;
+//     toURL(string: string): I_Chart;
+//     height(value: string | number): I_Chart;
+//     width(value: string | number): I_Chart;
+// }
+// export class Chart {
+//     ch: Chart;
+//     constructor(
+//         options?: ChartOptions | any
+//     ) {
+//         // @ts-ignore
+//         let c = new Chart1(options) as Chart;
+//         this.ch = c;
+//     }
+//     backgroundColor(value: string): Chart {
+//         return Chart.init(this.ch.backgroundColor(value))
+//     }
+//     bkg(value: string): Chart {
+//         return Chart.init(this.ch.bkg(value))
+//     }
+//     c(value: string): Chart {
+//         return Chart.init(this.ch.c(value))
+//     }
+//     encoding(value: string): Chart {
+//         return Chart.init(this.ch.encoding(value))
+//     }
+//     icac(value: string): Chart {
+//         return Chart.init(this.ch.icac(value))
+//     }
+//     ichm(value: string): Chart {
+//         return Chart.init(this.ch.ichm(value))
+//     }
+//     icretina(value: string): Chart {
+//         return Chart.init(this.ch.icretina(value))
+//     }
+//     toURL(value: string): Chart {
+//         return Chart.init(this.ch.toURL(value))
+//     }
+//     height(value: string | number): Chart {
+//         return Chart.init(this.ch.height(value))
+//     }
+//     width(value: string | number): Chart {
+//         return Chart.init(this.ch.width(value))
+//     }
+//     chart(value: string | any): Chart {
+//         return Chart.init(this.ch.chart(value))
+//     }
+//     toBuffer(): Promise<Buffer> {return this.ch.toBuffer() as Promise<Buffer>}
+//     toDataURI(string: string): Promise<string> {return this.ch.toDataURI(string) as Promise<string>}
+//     plot(data: number[][], title: string, color: string, width: number, height: number) {
+//         let c = this.chart(chartData(title, data.map(x => ({x: x[0], y: x[1]}))))
+//             .backgroundColor(color)
+//             .width(width)
+//             .height(height)
+//         return Chart.init(c);
+//     }
+//     async write(path: string) {
+//         fs.writeFileSync(path, await this.toBuffer());
+//     }
+//     private static init(c: Chart) {
+//         let chart = new Chart();
+//         chart.ch = c;
+//         return chart;
+//     }
+// }
 //# sourceMappingURL=chart.js.map

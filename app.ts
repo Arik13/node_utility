786b02a35527146da15e388fd16e278d523a7691
Arik Dicks <ariksven@gmail.com>
2022-04-26 16:16:00 -0700

    Removed dependency on ASSET_NAME field of directories

--- app.ts
+++ app.ts
@@ -1,13 +1,1 @@
 import "module-alias/register";
-
-// import { dirMapFromPath } from "./src/fse/DirMapInitializer";
-
-// dirMapFromPath("./test");
-
-import { copyDirectoryInto } from "@node/fse";
-
-import * as fs from "fs";
-let from = "./test/inner";
-let to = "./test2";
-fs.rmSync(to, { recursive: true, force: true });
-copyDirectoryInto(from, to);
--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("module-alias/register");
-// import { dirMapFromPath } from "./src/fse/DirMapInitializer";
-// dirMapFromPath("./test");
-const fse_1 = require("@node/fse");
-const fs = require("fs");
-let from = "./test/inner";
-let to = "./test2";
-fs.rmSync(to, { recursive: true, force: true });
-fse_1.copyDirectoryInto(from, to);
 //# sourceMappingURL=app.js.map
